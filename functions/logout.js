d252277ea207130ba3bdabb218deaaa8dc172d91
Corey H <github@thefamilyplan.xyz>
2025-07-08 11:02:40 -0800

    Delete functions/logout.js

--- functions/logout.js
+++
@@ -1,7 +0,0 @@
-export async function onRequestGet(context) {
-  return new Response("Logged out", {
-    headers: {
-      "Set-Cookie": "auth_user=; Path=/; Max-Age=0; HttpOnly; SameSite=Strict",
-    },
-  });
-}
