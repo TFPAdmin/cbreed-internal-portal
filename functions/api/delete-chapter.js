d4d7f2ff652eda51a6a58df71cfbd84a96e8642a
Corey H <github@thefamilyplan.xyz>
2025-07-08 22:24:18 -0800

    Update delete-chapter.js

--- functions/api/delete-chapter.js
+++ functions/api/delete-chapter.js
@@ -1,1 +1,21 @@
+export async function onRequestPost(context) {
+  try {
+    const DB = context.env.DB;
+    const { id } = await context.request.json();
+
+    if (!id) {
+      return new Response("Missing ID", { status: 400 });
+    }
+
+    await DB.prepare(`
+      DELETE FROM chapters
+      WHERE id = ?
+    `).bind(id).run();
+
+    return new Response("Chapter deleted", { status: 200 });
+  } catch (err) {
+    console.error("Delete Chapter Error:", err);
+    return new Response("Failed to delete chapter", { status: 500 });
+  }
+}
 
