376af7f8f4761c92e35ac007be717ad13b438d2f
Corey H <github@thefamilyplan.xyz>
2025-07-10 00:30:58 -0800

    Update get-books.js

--- functions/get-books.js
+++ functions/get-books.js
@@ -1,13 +1,15 @@
+// functions/get-books.js
+
 export async function onRequestGet(context) {
   try {
     const DB = context.env.DB;
 
     const { results } = await DB.prepare(`
-      SELECT id, title, subtitle, excerpt, cover, wattpad, 'published' AS status
-      FROM books
-      UNION ALL
-      SELECT id, title, subtitle, excerpt, cover, wattpad, 'wip' AS status
-      FROM wip_books
+      SELECT * FROM (
+        SELECT id, title, subtitle, excerpt, cover, wattpad, 'published' AS status FROM books
+        UNION ALL
+        SELECT id, title, subtitle, excerpt, cover, wattpad, 'wip' AS status FROM wip_books
+      )
       ORDER BY title ASC
     `).all();
 
