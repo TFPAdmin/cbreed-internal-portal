49801c7deeb0e8c407f167175b629a4e78b7f7e2
Corey H <github@thefamilyplan.xyz>
2025-07-13 21:01:56 -0800

    Update get-books.js

--- functions/get-books.js
+++ functions/get-books.js
@@ -1,1 +1,9 @@
-
+export async function onRequest({ env }) {
+  try {
+    const stmt = env.DB.prepare("SELECT * FROM books ORDER BY id DESC");
+    const books = await stmt.all();
+    return Response.json(books.results);
+  } catch (err) {
+    return new Response("Failed to get books", { status: 500 });
+  }
+}
