a3644b499d993645b7ceedf64fe9a2a4554241ed
Corey H <github@thefamilyplan.xyz>
2025-07-13 21:02:13 -0800

    Update add-book.js

--- functions/add-book.js
+++ functions/add-book.js
@@ -1,1 +1,16 @@
+export async function onRequest({ request, env }) {
+  try {
+    const data = await request.json();
+    const { title, subtitle, excerpt, cover, wattpad } = data;
 
+    const stmt = env.DB.prepare(`
+      INSERT INTO books (title, subtitle, excerpt, cover, wattpad)
+      VALUES (?, ?, ?, ?, ?)
+    `);
+
+    await stmt.bind(title, subtitle, excerpt, cover, wattpad).run();
+    return new Response("Book added", { status: 200 });
+  } catch (err) {
+    return new Response("Failed to add book", { status: 500 });
+  }
+}
