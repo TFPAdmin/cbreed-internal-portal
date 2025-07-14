b77d978d8cdf82300cb10b448596e89dca2f565c
Corey H <github@thefamilyplan.xyz>
2025-07-13 21:02:56 -0800

    Update get-chapters.js

--- functions/get-chapters.js
+++ functions/get-chapters.js
@@ -1,1 +1,16 @@
+export async function onRequest({ request, env }) {
+  try {
+    const url = new URL(request.url);
+    const bookTitle = url.searchParams.get("book_title");
 
+    const stmt = env.DB.prepare(`
+      SELECT * FROM chapters
+      WHERE book_title = ?
+      ORDER BY chapter_number ASC
+    `);
+    const chapters = await stmt.bind(bookTitle).all();
+    return Response.json(chapters.results);
+  } catch (err) {
+    return new Response("Failed to get chapters", { status: 500 });
+  }
+}
